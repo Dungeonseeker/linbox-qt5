9a76e4b79e26155dec43acd7fd1da40bcc2e22a2
xafero <georg.berendt@outlook.com>
2023-05-18 16:23:24 +0200

    Adding automatic creation of venv to start script

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-./venv/bin/python linbox.py
+
+if [ -d "./venv" ]; then
+  echo .
+else
+  python3 -m venv ./venv
+  ./venv/bin/pip install -r requirements.txt
+fi
+. ./venv/bin/activate
+
+python3 linbox.py
